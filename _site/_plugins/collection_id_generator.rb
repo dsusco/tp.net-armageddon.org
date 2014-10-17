9e15fcb25387aadabecfec2967b71ca56431e763
dsusco <dsusco@gmail.com>
2014-10-17 16:26:45 -0400

    getting rid of html_title and updating id generator to not overwrite an id in a file

--- _site/_plugins/collection_id_generator.rb
+++ _site/_plugins/collection_id_generator.rb
@@ -5,7 +5,7 @@ module Jekyll
     def generate(site)
       site.collections.each_value do |collection|
         collection.docs.each do |doc|
-          doc.data['id'] = File.basename(doc.path, File.extname(doc.path))
+          doc.data['id'] = File.basename(doc.path, File.extname(doc.path)) unless doc.data['id']
           doc.data['timestamp'] = doc.data['timestamp'].to_s unless doc.data['timestamp']
         end
       end
