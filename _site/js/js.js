c7ecc2ad4efa756b112fd2ef76bf54f35ee4bbef
dsusco <dsusco@gmail.com>
2014-10-17 15:18:38 -0400

    added JS to generate a toc

--- _site/js/js.js
+++ _site/js/js.js
@@ -1,6 +1,31 @@
 $(function () {
   'use strict';
 
+  var $toc = $('#table-of-contents > ol');
+
+  $('h1[id], h2[id], h3[id]').each(function () {
+    var $h = $(this),
+      $a = $('<a>', { href: '#' + $h.prop('id') }).text($h.text());
+
+    switch (this.tagName) {
+      case 'H1':
+        $a.addClass('_h1');
+        break;
+      case 'H2':
+        $a.addClass('_h2');
+        break;
+      case 'H3':
+        $a.addClass('_h3');
+        break;
+    }
+
+    if ($h.hasClass('no-count')) {
+      $a.addClass('no-count');
+    }
+
+    $toc.append($('<li>').append($a));
+  });
+
   $('.table-army-list:not(:has(.table-army-list)) > tbody').each(function () {
     // get the number of columns in each table body...
     var $tbody = $(this),
