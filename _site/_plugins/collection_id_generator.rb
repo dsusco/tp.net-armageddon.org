169782980de8083f0936e752411b198c9ea4db12
dsusco <dsusco@gmail.com>
2014-10-22 08:20:56 -0400

    renamed timestamp attr to date

--- _site/_plugins/collection_id_generator.rb
+++ _site/_plugins/collection_id_generator.rb
@@ -6,7 +6,7 @@ module Jekyll
       site.collections.each_value do |collection|
         collection.docs.each do |doc|
           doc.data['id'] = File.basename(doc.path, File.extname(doc.path)) unless doc.data['id']
-          doc.data['timestamp'] = doc.data['timestamp'].to_s unless doc.data['timestamp']
+          doc.data['date'] = doc.data['date'].to_s unless doc.data['date']
         end
       end
     end
