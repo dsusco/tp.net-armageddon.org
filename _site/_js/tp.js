2f043bfa2310f3561a7ba7e3c9a7fc14a0cfcacf
dsusco <dsusco@gmail.com>
2014-10-31 12:38:51 -0400

    added JS to change link text to heading #

--- _site/_js/tp.js
+++ _site/_js/tp.js
@@ -1,6 +1,12 @@
 $(function () {
   'use strict';
 
+  var
+    h,
+    h1 = 0,
+    h2 = 0,
+    h3 = 0;
+
   $('html').toggleClass('js no-js');
 
   if (typeof Prince !== 'undefined') {
@@ -11,6 +17,26 @@ $(function () {
     var $h = $(this),
       $a = $('<a>', { href: '#' + $h.prop('id') }).append($h.text());
 
+    switch (this.tagName) {
+      case 'H1':
+        h1 += 1;
+        h2 = 0;
+        h3 = 0;
+        h = h1 + '.0';
+        break;
+      case 'H2':
+        h2 += 1;
+        h3 = 0;
+        h = h1 + '.' + h2;
+        break;
+      case 'H3':
+        h3 += 1;
+        h = h1 + '.' + h2 + '.' + h3;
+        break;
+    }
+
+    $h.data('heading', h);
+
     $a.addClass(this.tagName.toLowerCase());
 
     if ($h.hasClass('no-count')) { $a.addClass('no-count'); }
@@ -18,6 +44,12 @@ $(function () {
     if ($h.hasClass('sr-only')) { $a.addClass('sr-only'); }
 
     $('#table-of-contents ol').append($('<li>').append($a));
+  });
+
+  $('main:not(.no-outline) section:not(#table-of-contents) a[href^="#"]').each(function () {
+    var $a = $(this);
+
+    this.innerHTML = $($a.attr('href')).data('heading');
   });
 
   $('.table-army-list:not(:has(.table-army-list)) > tbody').each(function () {
